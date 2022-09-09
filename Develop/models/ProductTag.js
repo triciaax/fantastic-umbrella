e00ed5b2217b5139042d74dfe5933ea491660e1f
Tricia <tricianghiem@gmail.com>
2022-09-09 10:57:28 -0500

    added modeling for product tag data

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,20 +1,33 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
+ProductTag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+
+  //foreign keys
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "product",
+      key: "id",
+    },
+  },
+
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "tag",
+      key: "id",
+    },
+  },
+});
 
 module.exports = ProductTag;
