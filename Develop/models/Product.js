5ee0ccbd1cf69c0081289b4792751b22dfe74c13
Tricia <tricianghiem@gmail.com>
2022-09-13 16:12:25 -0500

    updated db models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,6 +9,7 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -20,7 +21,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL_NUMBERS,
+      type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
         isDecimal: true,
@@ -29,22 +30,12 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
       validate: {
-        isInt: true,
+        isNumeric: true,
       },
     },
   },
-
-  // foreign key
-  category_id,
-  {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "category",
-      key: "id",
-    },
-  },
-
   {
     sequelize,
     timestamps: false,
