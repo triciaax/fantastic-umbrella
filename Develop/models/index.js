d9de0431a1ebc0fbc38c3583a48444476bcaa191
Tricia <tricianghiem@gmail.com>
2022-09-13 16:11:31 -0500

    added foreign key

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,25 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  onDelete: "CASCADE",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  onDelete: "CASCADE",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 module.exports = {
   Product,
   Category,
