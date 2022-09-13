f2d8bd4fe6f77a4fab4291aae40cb34dd528bde4
Tricia <tricianghiem@gmail.com>
2022-09-13 16:12:36 -0500

    updated product tag model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -4,30 +4,41 @@ const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
-ProductTag.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-  //foreign keys
-  product_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "product",
-      key: "id",
+    //foreign keys
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+      onDelete: "CASCADE",
     },
   },
-
-  tag_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "tag",
-      key: "id",
-    },
-  },
-});
+  {
+    sequelize: sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product_tags",
+  }
+);
 
 module.exports = ProductTag;
